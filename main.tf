d5c8481be64fe0d3fdd05f50a9dfb5eb4b4d7253
ShubhamSinghTX <130536532+ShubhamSinghTX@users.noreply.github.com>
2023-06-22 10:22:57 +0530

    Updated script

--- main.tf
+++ main.tf
@@ -4,8 +4,8 @@ provider "aws" {
 
 //s3
 
-resource "aws_s3_bucket" "shubhamsinghtx" {
-    bucket = "shubhamsinghtx"
+resource "aws_s3_bucket" "shubhanmsinghsingh" {
+    bucket = "shubhanmsinghsingh"
     acl = "private"
     
     versioning {
